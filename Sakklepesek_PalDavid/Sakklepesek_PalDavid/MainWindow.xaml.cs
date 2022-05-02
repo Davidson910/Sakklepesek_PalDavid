f21329783102f02b76e3c24607cb8f841aab8d80
Davidson910 <paldavid.0910@gmail.com>
2022-05-02 08:20:21 +0200

    Sakktábla elkészült
    Sakktábla legenárálása működik

--- Sakklepesek_PalDavid/Sakklepesek_PalDavid/MainWindow.xaml.cs
+++ Sakklepesek_PalDavid/Sakklepesek_PalDavid/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace Sakklepesek_PalDavid
     /// </summary>
     public partial class MainWindow : Window
     {
+        int meret = 8;
+        Rectangle[,] mezok;
         public MainWindow()
         {
             InitializeComponent();
+            Tablakialakitasa();
+        }
+        private void Tablakialakitasa()
+        {
+            mezok = new Rectangle[meret, meret];
+            for (int i = 0; i < meret; i++)
+            {
+
+                for (int j = 0; j < meret; j++)
+                {
+                    Button negyzet = new Button();
+                    negyzet.Height = tabla.Height / meret;
+                    negyzet.Width = tabla.Width / meret;
+                    negyzet.Margin = new Thickness(tabla.Width / meret * j, tabla.Height / meret * i, 0, 0);
+                    //negyzet.Click += SzinCsere;
+                    tabla.Children.Add(negyzet);
+
+                    if (i % 2 == 1 && j % 2 == 1)
+                        negyzet.Background = Brushes.White;
+                    else if (j % 2 == 0 && i % 2 == 0)
+                        negyzet.Background = Brushes.White;
+                    else
+                        negyzet.Background = Brushes.Black;
+                }
+            }
         }
     }
 }
